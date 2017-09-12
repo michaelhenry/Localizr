8608959bd33b9c681894f848bb2f5b58831d7342
Michael Henry Pantaleon <me@iamkel.net>
2017-09-13 00:50:46 +0900

    rename the project from Localizr to LocalizrExample

--- LocalizrExample/wsgi.py
+++ LocalizrExample/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Localizr project.
+WSGI config for LocalizrExample project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Localizr.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LocalizrExample.settings")
 
 application = get_wsgi_application()
