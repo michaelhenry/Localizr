2caf8cb6aca774d8cf60728029e4bee040ea6741
Michael Henry Pantaleon <me@iamkel.net>
2017-10-30 02:52:33 +0900

    added Procfile and default deployment script.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+python manage.py migrate
+
+cat <<EOF | python manage.py shell
+import os
+from django.contrib.auth.models import User
+if os.environ.get("ADMIN_USERNAME", None):
+	User.objects.filter(username="$ADMIN_USERNAME").exists() or \
+		User.objects.create_superuser("$ADMIN_USERNAME", "$ADMIN_EMAIL", "$ADMIN_PASSWORD")
+EOF
+
+python manage.py collectstatic -v0 --noinput
+gunicorn LocalizrExample.wsgi
