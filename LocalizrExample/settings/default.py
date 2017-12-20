621c4fb3c685c2e24001d4ede896d99e1308098e
Michael Henry Pantaleon <me@iamkel.net>
2017-12-21 02:16:23 +0900

    allowed Authenticated or Options request for react.

--- LocalizrExample/settings/default.py
+++ LocalizrExample/settings/default.py
@@ -134,7 +134,7 @@ MEDIA_URL  = '/media/'
 REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'LocalizrExample.permissions.IsAuthenticatedOrOptionsRequest',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
